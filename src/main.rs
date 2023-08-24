2e5ddefe3655037018cca0ea391aac592b71c759
Alex Wellnitz <alex.wellnitz@materna.group>
2023-08-25 00:31:07 +0200

    Get all hostnames from ingresses

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,44 @@ use kube::{Client, Api, api::ListParams};
 
 type Result<T, E = anyhow::Error> = std::result::Result<T, E>;
 
+const INGRESSCONTROLLER: &str = "ingress-contour-envoy.projectcontour.svc.cluster.local";
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let client = Client::try_default().await?;
     let ingress_api: Api<Ingress> = Api::all(client.to_owned());
     let config_api: Api<ConfigMap> = Api::namespaced(client.to_owned(), "kube-system");
-    let pod_api: Api<Pod> = Api::namespaced(client.to_owned(), "kube-system");
   
     // Get all ingresses
     let ingresses = ingress_api.list(&Default::default()).await?;
+
+    // Save ingress hostnames in a vector
+    let mut hostnames: Vec<String> = Vec::new();
     for ingress in ingresses.items {
-        println!("Ingress {} in namespace {}", ingress.metadata.name.unwrap(), ingress.metadata.namespace.unwrap());
+        let hostnames_rules = ingress.spec.unwrap().rules.unwrap();
+        for hostname in hostnames_rules {
+            let host = hostname.host.unwrap();
+            hostnames.push(host);
+        }
     }
+    println!("Hostnames: {:?}", hostnames);
 
     // Get config map in kube-system namespace with name coredns
     let config_map = config_api.get("coredns").await?;
     println!("Config map {} in namespace {}", config_map.metadata.name.unwrap(), config_map.metadata.namespace.unwrap());
 
-    // Get pod in kube-system namespace with label k8s-app: kube-dns
-    let pods = pod_api.list(&ListParams::default().labels("k8s-app=kube-dns")).await?;
-    for pod in &pods.items {
-        println!("Pod {} in namespace {}", pod.metadata.name.to_owned().unwrap(), pod.metadata.namespace.to_owned().unwrap());
-    }
+    // Restart CoreDNS after patching config map
+    restart_coredns(client.to_owned()).await?;
 
-    // delete pods
+    Ok(())
+}
+
+async fn restart_coredns(client: Client) -> Result<()> {
+    let pod_api: Api<Pod> = Api::namespaced(client.to_owned(), "kube-system");
+    let pods = pod_api.list(&ListParams::default().labels("k8s-app=kube-dns")).await?;
     for pod in pods.items {
+        println!("Restarting CoreDNS");
         pod_api.delete(&pod.metadata.name.unwrap(), &Default::default()).await?;
     }
-
     Ok(())
 }
